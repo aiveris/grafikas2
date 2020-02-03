264a28980bb13365e0d08751cfe3e714e4b6caa5
aiveris <aiveris@gmail.com>
2020-02-03 12:33:22 +0200

    2020-01-31

---
+++ src/Authentification.java
@@ -0,0 +1,5 @@
+
+
+public class Authentification extends javax.swing.JFrame {
+
+}
---
+++ src/Login.java
@@ -0,0 +1,6 @@
+
+import java.util.Scanner; // I use scanner because it's command line.
+
+public class Login {
+
+}
